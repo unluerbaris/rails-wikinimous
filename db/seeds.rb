a796ff8abaf8e38099c765d571a183db1f587dd9
Baris Unluer <unluerbaris@gmail.com>
2020-05-12 15:10:38 +0900

    added data from faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  article = Article.new(
+    title:    "#{Faker::Creature::Animal.name} eats #{Faker::Food.dish}",
+    content: "and says can I add #{Faker::Food.measurement} #{Faker::Food.ingredient}"
+  )
+  article.save!
+end
